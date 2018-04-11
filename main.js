f3b79c19f339a9b6c7c5bf3a3ada8d4fe0716b16
Darin Estrin <darin.estrin@gmail.com>
2018-04-10 19:50:07 -0700

    calculator adds operators when clicked and if two operators are clicked in a row, the most recent one clicked will be the operator used.

--- main.js
+++ main.js
@@ -6,7 +6,11 @@ $(document).ready(function() {
   $('.clear').on('click', calculator.clear);
 
   $('.number').on('click', function(e) {
-    calculator.storeNumber(e.target.innerText);
+    calculator.numberClicked(e.target.innerText);
+  });
+
+  $('.operator').on('click', function(e) {
+    calculator.operatorClicked(e.target.innerText);
   });
 
 });
@@ -32,18 +36,53 @@ function Calculator() {
    * @method 
    * adds the last number clicked to the end of the current number
    */
-  this.storeNumber = function(number) {
+  this.numberClicked = function(number) {
     var currentIndex;
-    numbers.length > 0 ? currentIndex = numbers.length-1 : 0;
+    numbers.length > 0 ? currentIndex = numbers.length : currentIndex = 0;
     if (numbers[currentIndex] === undefined && +number === 0) {
       return;
-    } else if (numbers[currentIndex] === undefined) {
-      numbers[currentIndex] = number;
+    } else if (!isNaN(numbers[currentIndex-1])) {
+      numbers[currentIndex-1] += number;
+      $('.current-number').text(numbers[currentIndex-1]);
     } else {
-      numbers[currentIndex] += number;
+      numbers[currentIndex] = number;
+      $('.current-number').text(numbers[currentIndex]);
+    }
+  }
+
+  /**
+   * 
+   * @param {string} operator 
+   * operator clicked on calculator
+   * @method
+   * creates a new number and adds the operator the end of the calculation
+   */
+  this.operatorClicked = function(operator) {
+    var calculation = '';
+    if (numbers.length === 0) {
+      return;
     }
 
-    $('.current-number').text(numbers[currentIndex]);
+    switch (operator) {
+      case '+':
+      case '-':
+      case '/':
+      case '*':
+        if (!isNaN(numbers[numbers.length-1])) {
+          numbers.push(operator);
+        } else {
+          numbers[numbers.length-1] = operator;
+        }
+        break;
+      default:
+        return;
+    }
+
+    numbers.forEach(function(number) {
+      calculation += (number + ' ');
+    });
+    $('.staged-calculation').text(calculation);
+    $('.current-number').text('0');
   }
 
 }
